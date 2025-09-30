53b1f6ed7b23928ae6a5cf1a8a8d76fec92f591c
Zein <zeinkasar123@gmail.com>
2025-10-01 00:30:54 +0200

    Added Unit tests

--- trivia-backend/src/test/java/trivia_backend/trivia_backend/controller/QuestionControllerTest.java
+++ trivia-backend/src/test/java/trivia_backend/trivia_backend/controller/QuestionControllerTest.java
@@ -1,4 +1,83 @@
 package trivia_backend.trivia_backend.controller;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import trivia_backend.trivia_backend.dto.CheckAnswerRequest;
+import trivia_backend.trivia_backend.dto.CheckAnswerResponse;
+import trivia_backend.trivia_backend.model.Answer;
+import trivia_backend.trivia_backend.model.AnswerResult;
+import trivia_backend.trivia_backend.model.Question;
+import trivia_backend.trivia_backend.service.QuestionService;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@ExtendWith(MockitoExtension.class)
 public class QuestionControllerTest {
+
+    @Mock
+    private QuestionService service;
+
+    @InjectMocks
+    private QuestionController controller;
+
+    @Test
+    void getQuestion_shouldReturnQuestions() {
+        //Arrange
+        int amount = 2;
+        int category = 9;
+        String difficulty = "easy";
+        String sessionId = "test-session";
+
+        Question question1 = new Question(UUID.randomUUID(), "Test Question1", List.of("Option1", "Option2", "Option3", "Option4"));
+        Question question2 = new Question(UUID.randomUUID(), "Test Question2", List.of("Option1", "Option2", "Option3", "Option4"));
+
+        List<Question> questionList = new ArrayList<>();
+        questionList.add(question1);
+        questionList.add(question2);
+
+        when(service.getQuestions(amount, category, difficulty, sessionId)).thenReturn(questionList);
+
+        //Act
+        ResponseEntity<List<Question>> response = controller.getQuestions(amount, category, difficulty, sessionId);
+
+        //Assert
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+        assertNotNull(response.getBody());
+        assertEquals(2, response.getBody().size());
+    }
+
+    @Test
+    void checkAnswers_shouldReturnCheckedAnswers() {
+        //Arrange
+        Answer answer1 = new Answer(UUID.randomUUID(), "Test Answer1");
+        Answer answer2 = new Answer(UUID.randomUUID(), "Test Answer2");
+        List<Answer> answers = new ArrayList<>();
+        answers.add(answer1);
+        answers.add(answer2);
+
+        CheckAnswerRequest request = new CheckAnswerRequest(answers);
+        AnswerResult result1 = new AnswerResult(answer1.getQuestionId(), "Test Question1","Correct Answer1", "Correct Answer1", true);
+        AnswerResult result2 = new AnswerResult(answer2.getQuestionId(), "Test Question2", "Wrong Answer2", "Correct Answer2", false);
+        CheckAnswerResponse response = new CheckAnswerResponse(1, 2, List.of(result1, result2));
+
+        when(service.checkAnswers(request)).thenReturn(response);
+
+        //Act
+        ResponseEntity<CheckAnswerResponse> controllerResponse = controller.checkAnswers(request);
+
+        //Assert
+          assertTrue(controllerResponse.getStatusCode().is2xxSuccessful());
+          assertNotNull(controllerResponse.getBody());
+          assertEquals(1, controllerResponse.getBody().getScore());
+          assertEquals(2, controllerResponse.getBody().getTotal());
+          assertEquals(2, controllerResponse.getBody().getResults().size());
+    }
 }
--- trivia-backend/src/test/java/trivia_backend/trivia_backend/service/QuestionServiceTest.java
+++ trivia-backend/src/test/java/trivia_backend/trivia_backend/service/QuestionServiceTest.java
@@ -1,4 +1,80 @@
 package trivia_backend.trivia_backend.service;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import trivia_backend.trivia_backend.dto.CheckAnswerRequest;
+import trivia_backend.trivia_backend.dto.CheckAnswerResponse;
+import trivia_backend.trivia_backend.model.Answer;
+import trivia_backend.trivia_backend.model.Question;
+import trivia_backend.trivia_backend.openTrivia.OpenTriviaQuestion;
+import trivia_backend.trivia_backend.openTrivia.OpenTriviaService;
+import trivia_backend.trivia_backend.openTrivia.dto.OpenTriviaResponse;
+import trivia_backend.trivia_backend.repository.AnswerRepository;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class QuestionServiceTest {
+
+    @Mock
+    private AnswerRepository answerRepository;
+
+    @Mock
+    private OpenTriviaService openTriviaService;
+
+    @InjectMocks
+    private QuestionService questionService;
+
+    @Test
+    void getQuestion_shouldReturnQuestions() {
+        //Arrange
+        int amount = 2;
+        Integer category = 9;
+        String difficulty = "easy";
+        String sessionId = "test-session";
+        List<OpenTriviaQuestion> results = List.of(
+                new OpenTriviaQuestion("Any Category", "Multi", "easy", "Test Question1", "Correct Answer1", List.of("Incorrect1", "Incorrect2", "Incorrect3")),
+                new OpenTriviaQuestion("Any Category", "Multi", "easy", "Test Question2", "Correct Answer2", List.of("Incorrect1", "Incorrect2", "Incorrect3"))
+        );
+        OpenTriviaResponse apiResponse = new OpenTriviaResponse(0, results);
+
+        when(openTriviaService.fetchQuestions(amount, category, difficulty, sessionId)).thenReturn(apiResponse);
+        //Act
+        List<Question> questions = questionService.getQuestions(amount, category, difficulty, sessionId);
+        //Assert
+        assertEquals(2, questions.size());
+        verify(answerRepository, times(2)).save(any(), any(), any());
+    }
+
+    @Test
+    void checkAnswers_shouldReturnCheckedAnswers() {
+        //Arrange
+        UUID questionId1 = UUID.randomUUID();
+        String correctAnswer1 = "Correct Answer1";
+        UUID questionId2 = UUID.randomUUID();
+        String correctAnswer2 = "Correct Answer2";
+
+        CheckAnswerRequest request = new CheckAnswerRequest(List.of(
+                new Answer(questionId1, "Correct Answer1"),
+                new Answer(questionId2, "Wrong Answer2")
+        ));
+
+        when(answerRepository.getAnswer(questionId1)).thenReturn(correctAnswer1);
+        when(answerRepository.getAnswer(questionId2)).thenReturn(correctAnswer2);
+
+        //Act
+        CheckAnswerResponse response = questionService.checkAnswers(request);
+
+        //Assert
+        assertEquals(1, response.getScore());
+        assertEquals(2, response.getTotal());
+        assertEquals(2, response.getResults().size());
+    }
 }
